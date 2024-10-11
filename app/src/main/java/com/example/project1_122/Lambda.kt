b1d1e0d36134b92f958a414b6e3a36fc5d362593
mrendy58 <derendyyy58@gmail.com>
2024-10-11 22:26:53 +0700

    membuat fungsi sederhana

--- app/src/main/java/com/example/project1_122/Lambda.kt
+++ app/src/main/java/com/example/project1_122/Lambda.kt
@@ -8,7 +8,6 @@ package com.example.project1_122
 fun uppercaseString(string: String): String {
     return string.uppercase()
 }
-
 //Dapat ditulis dalam ekspresi lambda sbb :
 fun main() {
     uppercaseString("hello")
