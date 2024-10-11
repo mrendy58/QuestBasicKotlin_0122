cb53f0a99f491818cc72e96e0b4e1633147b2ca4
mrendy58 <derendyyy58@gmail.com>
2024-10-11 23:02:34 +0700

    membuat fungsi dengan menyebutkan nama

--- app/src/main/java/com/example/project1_122/Functions.kt
+++ app/src/main/java/com/example/project1_122/Functions.kt
@@ -11,3 +11,10 @@ fun withParameter(name: String) {
     println("Hello, $name!")
 }
 
+// Named argument
+// Named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
