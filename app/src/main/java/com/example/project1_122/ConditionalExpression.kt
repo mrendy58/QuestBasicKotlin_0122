19d923fd78f478f79e62bbe0ac695dfaf366e7d1
mrendy58 <derendyyy58@gmail.com>
2024-10-11 22:53:57 +0700

    contoh ekspresi dengan kondisi Try-Catch

--- app/src/main/java/com/example/project1_122/ConditionalExpression.kt
+++ app/src/main/java/com/example/project1_122/ConditionalExpression.kt
@@ -27,5 +27,43 @@ fun ConditionalStatement() {
         else -> println("Invalid Day")
     }
 
+// Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid number")
+    }
+}
 
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+fun main() {
+    ConditionalStatement()
+    Perulangan()
 }
